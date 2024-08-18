19b1d3519bb072a618e3955096347027e5729548
saurabh4323 <saurabhiitr01@gmail.com>
2024-08-18 15:49:57 +0530

    DSA

--- src/Events/Event.jsx
+++ src/Events/Event.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Header from "../Header/Header";
-
+import "./eve.css";
 const Event = () => {
   return (
     <div>
       <Header></Header>
+      <div className="container">
+        <div className="col">
+          <h1>Events Of Cipher</h1>
+          <div className="card">
+            <img src="eve.png"></img>
+          </div>
+          <div className="card">
+            <img src="ev.png"></img>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
